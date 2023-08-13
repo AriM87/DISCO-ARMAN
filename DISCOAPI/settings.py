ca763e7e4bc165281a9e414f9356c22bb6dc1c21
AriM87 <armanmehrsa@gmail.com>
2023-08-13 21:16:07 +1000

    setting.py updated with installed apps and AUTH_USER_MODEL

--- DISCOAPI/settings.py
+++ DISCOAPI/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'ImageHostingAPI'
+    'rest_framework_nested',
+    'ImageHostingAPI',
+    'imagekit',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'ImageHostingAPI.CustomUser'
